d749ad5a0946a6f06de983e8d94e722ef620bdd1
Borna Blazevic <borna.blazevic@sartura.hr>
2021-07-30 14:13:21 +0200

    add a wrapper for named pipes
    Signed-off-by: Borna Blazevic <borna.blazevic@sartura.hr>

--- src/utils/src/lib.rs
+++ src/utils/src/lib.rs
@@ -1,8 +1,13 @@
 // Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
+use std::ffi::CString;
+use std::path::Path;
+use std::{io, result};
+
 pub mod resource_download;
 
+// -----------------------------------------------------------------------------
 /// A macro for printing errors only in debug mode.
 #[macro_export]
 #[cfg(debug_assertions)]
@@ -17,4 +22,31 @@ macro_rules! debug {
     ($( $args:expr ),*) => {
         ()
     };
+}
+
+// -----------------------------------------------------------------------------
+/// Enum containing error types.
+pub enum Error {
+    Io(io::Error),
+    Path,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Erorr: {}", (*self))
+    }
+}
+
+/// A safe wrapper around libc::mkfifo().
+pub fn mkfifo<P: AsRef<Path>>(fifo_path: P, mode: u32) -> result::Result<(), Error> {
+    let fifo_path_str = fifo_path.as_ref().to_str().ok_or(Error::Path)?;
+    let fifo_path_cstr = CString::new(fifo_path_str).unwrap();
+
+    let ret = unsafe { libc::mkfifo(fifo_path_cstr.as_ptr(), mode) };
+
+    if ret != 0 {
+        Err(Error::Io(io::Error::last_os_error()))
+    } else {
+        Ok(())
+    }
 }
